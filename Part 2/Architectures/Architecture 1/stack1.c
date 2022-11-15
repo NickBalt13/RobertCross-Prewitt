35e8a7f7de240fc3ba6e00e9bbbee53010658b36
Nikolaos Baltagiannis <118275920+NickBalt13@users.noreply.github.com>
2022-11-15 17:00:24 +0200

    Add files via upload

---
+++ Part 2/Architectures/Architecture 1/stack1.c
@@ -0,0 +1,23 @@
+#include <rt_misc.h>
+
+ __value_in_regs struct __initial_stackheap __user_initial_stackheap( 
+ unsigned R0, unsigned SP, unsigned R2, unsigned SL){    
+ 
+ struct __initial_stackheap config;     
+
+ //config.heap_limit  = 0x00724B40;     
+
+ //config.stack_limit = 0x00004000;
+ //config.stack_limit = 0x00100000;    
+
+/* works */
+config.heap_base   = 0x7E80000;	 	 
+config.stack_base  = 0x8080000;       
+
+/* factory defaults */
+ //config.heap_base   = 0x00060000;     
+ //config.stack_base  = 0x00080000;    
+ return config;} 
+ 
+ 
+ 
