0ec71089a2bf522d8277287052db5e546f571ef1
Nikolaos Baltagiannis <118275920+NickBalt13@users.noreply.github.com>
2022-11-15 17:02:24 +0200

    Add files via upload

---
+++ Part 2/Architectures/Architecture 3/stack3.c
@@ -0,0 +1,24 @@
+#include <rt_misc.h>
+
+ __value_in_regs struct __initial_stackheap __user_initial_stackheap( 
+ unsigned R0, unsigned SP, unsigned R2, unsigned SL){    
+ 
+ struct __initial_stackheap config;     
+
+ //config.heap_limit  = 0x00724B40;     
+
+ //config.stack_limit = 0x00004000;
+ //config.stack_limit = 0x00100000;    
+
+/* works */
+config.heap_base   = 0x5132A4;	 	 // - 2000 bytes form heap's start
+config.stack_base  = 0x513A74;       // where .map RAM + ROM ends
+
+
+/* factory defaults */
+ //config.heap_base   = 0x00060000;     
+ //config.stack_base  = 0x00080000;    
+ return config;} 
+ 
+ 
+ 
